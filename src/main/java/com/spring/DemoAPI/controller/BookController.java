f0f2856fa5ced4abbda9f7e7abc14013260943d6
AnhTu <3artemis98@gmail.com>
2023-09-08 16:39:52 +0700

    chuc nang update

--- src/main/java/com/spring/DemoAPI/controller/BookController.java
+++ src/main/java/com/spring/DemoAPI/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,12 @@ public Book createBook(@RequestBody RequestBook bookRequest) {
 @GetMapping(value="/{id}")
 public Book getBookById(@PathVariable("id") String id) {
 	return books.get(id);
+}
+
+@PutMapping(value="/{id}")
+public Book updateBook(@PathVariable("id")String id,@RequestBody RequestBook bookRequest) {
+	Book updateBook=new Book(id,bookRequest.title(),bookRequest.author(),bookRequest.year());
+	books.put(id, updateBook);
+	return updateBook;
 }
 }
